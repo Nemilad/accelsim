d3ceb47a65eb42cf7892ce36521b231ccde2f83b
Nemilad <Nik155@list.ru>
2021-01-27 15:58:42 +0300

    tab change brython implementation

---
+++ app.py
@@ -0,0 +1,12 @@
+from browser import document, bind, alert
+
+@bind("button.tab", "click")
+def switch_tab(ev):
+    button = document[ev.target.id]
+    tab = document[ev.target.id[:-7]]
+    for x in document.select("div.tabcontent"):
+        document[x.id].style.display = "none"
+    for x in document.select("button.tab"):
+        document[x.id].class_name = document[x.id].class_name.replace(" active", "")
+    tab.style.display = "block"
+    button.class_name += " active"
